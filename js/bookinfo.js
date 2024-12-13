ed06b93091c84a7583496ce5c35afdd5cb8fd221
LeeYunSeok <seok000908@gmail.com>
2024-12-14 03:46:49 +0900

    1214

--- js/bookinfo.js
+++ js/bookinfo.js
@@ -8,8 +8,8 @@ function displayBookDetail() {
     }
 
     const bookDetailsContainer = document.getElementById('book-details');
-    bookDetailsContainer.innerHTML = `
-      <img src="${bookDetail.image}" alt="${bookDetail.title}">
+    bookDetailsContainer.innerHTML = /* html */`
+      <!-- <img src="${bookDetail.image}" alt="${bookDetail.title}">
       <h3>${bookDetail.title}</h3>
       <p><strong>Author:</strong> ${bookDetail.author}</p>
       <p><strong>Publisher:</strong> ${bookDetail.publisher}</p>
@@ -17,8 +17,32 @@ function displayBookDetail() {
       <p><strong>Price:</strong> ${bookDetail.price}원</p>
       <p><strong>Genre:</strong> ${bookDetail.genre}</p>
       <h4>Summary:</h4>
-      <p>${bookDetail.summary}</p>
+      <p>${bookDetail.summary}</p> -->
+
+      <main class="detail-page">
+        <div class="detail-container">
+          <div class="detail-goods-photo">
+            <img src="${bookDetail.image}" alt="${bookDetail.title}" class="detail-photo" />
+          </div>
+          <div class="detail-info-box">
+            <div class="detail-goods">
+              <dl class="detail-info">
+                <dt class="detail-name">${bookDetail.title}</dt>
+                <dt class="detail-writer">${bookDetail.author}</dt>
+                <dd class="detail-price">${bookDetail.price}원</dd>
+              </dl>
+              <p class="detail-summary"></p>
+            </div>
+            <div class="detail-info-control">
+              <button type="button" class="control-btn detail-wish">WISH</button>
+              <button type="button" class="control-btn detail-cart">CART</button>
+            </div>
+          </div>
+        </div>
+      </main>
     `;
+
+
   } catch (error) {
     document.getElementById('book-details').innerHTML = `<p style="color: red;">Error: ${error.message}</p>`;
   }
