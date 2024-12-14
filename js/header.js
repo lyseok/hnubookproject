6b2451a498d3c5db27983fbc7dbed87438603e81
LeeYunSeok <seok000908@gmail.com>
2024-12-14 23:45:33 +0900

    header merge

--- js/header.js
+++ js/header.js
@@ -1,3 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const header = document.querySelector('header[data-include-path]');
+  const path = header.getAttribute('data-include-path');
+
+  if (path) {
+      fetch(path)
+          .then(response => response.text())
+          .then(data => {
+              header.innerHTML = data; // 외부 파일 내용을 삽입
+          })
+          .catch(err => console.error('Error loading header:', err));
+  }
+});
+
+
 $('#header').prepend('<div id="menu-icon"><span class="first"></span><span class="second"></span><span class="third"></span></div>');
 	
 $("#menu-icon").on("click", function(){
